d576e3fd24220a0d8ef5f58794534d91ea93c23b
Jason <nba29@txstate.edu>
2026-04-18 16:06:48 -0500

    Add drag and drop piece movement logic

--- src/gui/ChessWindow.java
+++ src/gui/ChessWindow.java
@@ -4,11 +4,24 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.GridLayout;
 import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 /**
  * The main GUI window for the chess game
  * Displays the 8x8 chess board
  */
 public class ChessWindow extends JFrame {
+
+    /**
+     * The piece being dragged
+     */
+    private JLabel draggedPiece = null;
+
+    /**
+     * The start of the dragging
+     */
+    private JPanel startingSquare = null;
+
     //constructor
     public ChessWindow() {
         //sets the size of the window to 8x8 squares
@@ -25,8 +38,6 @@ public class ChessWindow extends JFrame {
             else{
                 square.setBackground(new java.awt.Color(118, 150, 86));
             }
-            //adds the square to the window
-            this.add(square);
 
             /**
              * An array of the starting pieces
@@ -48,8 +59,46 @@ public class ChessWindow extends JFrame {
             JLabel pieceSticker = new JLabel(startingPieces[i]); 
             pieceSticker.setFont(new java.awt.Font("Serif", java.awt.Font.PLAIN, 40));
             square.add(pieceSticker);
+
+            /**
+             * mouse listener. Handles dragging of pieces
+             */
+            square.addMouseListener(new MouseAdapter() {
+                public void mousePressed(MouseEvent e){
+                    startingSquare = (JPanel) e.getSource();
+                    //grabs piece if square has a piece
+                    if(startingSquare.getComponentCount() > 0){
+                        draggedPiece = (JLabel) startingSquare.getComponent(0);
+                    }
+                }
+
+                public void mouseReleased(MouseEvent e){
+                    if (draggedPiece != null) {
+                        //finds the specific desitination square
+                        java.awt.Component droopedOn = ChessWindow.this.getContentPane().getComponentAt(javax.swing.SwingUtilities.convertPoint(startingSquare, e.getPoint(), ChessWindow.this.getContentPane()));
+                        if(droopedOn instanceof JPanel){
+                            JPanel targetSquare = (JPanel) droopedOn;
+                        //remove any pieces that are captured then add our piece
+                        targetSquare.removeAll();
+                        targetSquare.add(draggedPiece);
+                        
+                        // refresh both squares visually
+                        targetSquare.revalidate();
+                        targetSquare.repaint();
+                        startingSquare.revalidate();
+                        startingSquare.repaint();
+                        }
+                    }
+                    //clear the dragged piece memory
+                    draggedPiece = null;
+                }
+            });
+
+            //adds the square to the window
+            this.add(square);
         }
     }
+
     public static void main(String[] args){
         ChessWindow window = new ChessWindow();
         window.setSize(600, 600);
