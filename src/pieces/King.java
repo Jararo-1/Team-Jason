a60b50ce710a33ff9e8320bbed7ecbb99c69b955
Jason <nba29@txstate.edu>
2026-05-01 16:42:48 -0500

    Add isValidMove logic for king

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -25,4 +25,33 @@ public class King extends Piece {
         return new ArrayList<>();
     }
 
+    /**
+     * Validates the king's moves
+     * @param board The current state of the board
+     * @param end The target position
+     * @return true if the move is valid false otherwise
+     */
+    @Override
+    public boolean isValidMove(board.Board board, Position end) {
+        int rowDiff = Math.abs(this.position.getRow() - end.getRow());
+        int colDiff = Math.abs(this.position.getCol() - end.getCol());
+
+    //The king moves one square in any direction
+    if(rowDiff > 1 || colDiff > 1){
+        return false;
+    }
+
+    //The king cannot move to the square it is already on
+    if(rowDiff == 0 && colDiff == 0){
+        return false;
+    }
+
+    // Make sure the king does not land on our own piece
+    pieces.Piece targetPiece = board.getPiece(end);
+    if(targetPiece != null && targetPiece.getColor() == this.color){
+        return false;
+    }
+    
+    return true;
+    }
 }
