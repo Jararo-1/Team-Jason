4e530c33ced89285ad371945b09402ffebc8dd03
Jason <nba29@txstate.edu>
2026-05-01 16:56:00 -0500

    Add isValidMove logic for bishop

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -23,4 +23,44 @@ public class Bishop extends Piece {
         // not implemented yet
         return new ArrayList<>();
     }
+
+    /**
+     * Validates the bishop's moves
+     * @param board The current state of the board
+     * @param end The target position
+     * @return true if the move is valid and unblocked false otherwise
+     */
+    @Override
+    public boolean isValidMove(board.Board board, Position end) {
+        int startRow = this.position.getRow();
+        int startCol = this.position.getCol();
+        int endRow = end.getRow();
+        int endCol = end.getCol();
+
+        // bishop moves diagonally (change in row must equal change in column)
+        if(Math.abs(startRow- endRow) != Math.abs(startCol - endCol)){
+            return false;
+        }
+
+        int rowStep = Integer.compare(endRow, startRow);
+        int colStep = Integer.compare(endCol, startCol);
+
+        //check if there is no piece along the path
+        int currentRow = startRow + rowStep;
+        int currentCol = startCol + colStep;
+        while(currentRow != endRow || currentCol != endCol) {
+            if(board.getPiece(new utils.Position(currentRow, currentCol)) != null){
+                return false;
+            }
+            currentRow += rowStep;
+            currentCol += colStep;
+        }
+
+        // make sure the bishop doesn't land on our own piece
+        pieces.Piece targetPiece = board.getPiece(end);
+        if(targetPiece != null && targetPiece.getColor() == this.color){
+            return false;
+        }
+        return true;
+    }
 }
